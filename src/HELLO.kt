aa3d6c6d5da17cae54ec1395b9b61fb5bc8b5972
Wuanitawanjiku <wanjikuwuanita@gmail.com>
2021-06-14 22:23:52 +0300

    second commit

--- src/HELLO.kt
+++ src/HELLO.kt
@@ -1,18 +1,48 @@
-fun main (){
-    var result =add(10,20)
-    print(result)
-}
-fun add(){
-    var a =10
-    var b =20
-    var add =(a+b)
-    println(add)
-}
-fun world(){
-    var name= "Wuanita"
-    println(name)
-}
-fun add(a:Int,b:Int):Int {
-    var sum = a + b
-    return (sum)
-}
+//You are creating an app to capture student records. Some of the data you will
+//capture includes full name, age, phone number, weight in kg, and citizen. For the citizen field, you will track whether a student is a Kenyan or not.
+//Create variables and assign them some values for each of these fields.
+//
+//Given a string “Programming”. Write a function that prints out a string composed
+//of the first, third, and fourth characters of the string
+//
+//Write a function that takes in 2 parameters, name and age, and returns a
+//String with this structure “Hi, my name is x and I am y years old.” Where x and
+//y are the provided name and age respectively.
+//
+//Create a class called Human with these attributes: name, age, weight. It has
+//the following functions:
+//- eat(foodWeight: Int) :Prints “I am eating {foodWeight} kgs of food”
+//and increments the human’s weight by the weight of the food eaten
+//
+//- speak(speech: String): Prints the string passed to it (2
+//points)
+//- birthday( ) :Increments the human’s age by 1 (2
+//points)
+//Create an instance of this human class and invoke all its functions
+//
+//
+//Create a data class User with these fields: firstName, lastName, email,
+//phoneNumber, password. Create an instance of User and print out any 2
+//attributes
+//
+//
+//Create a function that takes in 4 strings and creates an array out of them then
+//prints out the array.
+//
+//Create a function that given an array below:
+//var cities = arrayOf(“nairobi”, “kiambu”, “nakuru”, “kisumu”)
+//prints out the names of the cities in the correct grammatical case.
+//
+//Create one function that given the array below:
+//var numbers = arrayOf(32, 17,4,213,78,43,90,121, 244,31,3,73,11,345,158,62)
+//(i) prints out the sum of the second and fifth elements
+//(ii) prints out the index of 158
+//(iii) prints out the elements in ascending order
+//
+//Create a function that takes in 3 names and returns a string array containing
+//all 3 names.
+//
+//Write a function that takes in a string and returns its length
+//
+//Write a function that takes in a name and prints out “It's me!” when your
+//name is passed to it, otherwise, it prints out “I don’t know who that is”
